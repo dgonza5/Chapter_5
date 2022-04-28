633a33d5e0af4e4d946e63acbe34a70d42e1d7db
Deeno Gonzalez <deenogonzalez@swuser-129-8-235-119.swlan.csufresno.edu>
2022-04-28 15:07:47 -0700

    open and writing file

--- 5_1.py
+++ 5_1.py
@@ -3,25 +3,48 @@ file_name = "./infile.txt"
 infile = open(file_name, "r")
 names = [line.rstrip() for line in infile]
 
-infile.close()
+# infile.close()
 
-def main():
-    display_with_forloop(file_name)
-    print()
-def display_with_list_comprehension(file_name):
+# def main():
+#     display_with_forloop(file_name)
+#     print()
 
-def display_with_forloop(file_name):
-    infile = open_file(file_name, "r")
-    for line in infile:
-        print(line, end="")
-    infile.close()
 
-def display_with_list_comprehension(file_name):
-    infile = open_file(file_name, "r")
-    items = [line.rstrip() for line in infile]
-    print("items: ", items)
-    infile.close()
+# def display_with_forloop(file_name):
+#     infile = open_file(file_name, "r")
+#     for line in infile:
+#         print(line, end="")
+#     infile.close()
 
-def open_file(file_name, mode):
-    return open(file_name, mode)
-main()
+# def display_with_list_comprehension(file_name):
+#     infile = open_file(file_name, "r")
+#     items = [line.rstrip() for line in infile]
+#     print("items: ", items)
+#     infile.close()
+
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+# main()
+
+
+# Create a file object in write mode
+
+# def main():
+#     save_to_outfile("./outfile.txt")
+
+
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+
+
+# def save_to_outfile(file_name):
+#     outfile = open_file(file_name, "w")
+#     for name in names:
+#         if "Doe" not in name:
+#             print("Name to be presisted: ", name)
+#             outfile.write(name + "\n")
+#         else:
+#             print("Names to be exluded: ", name)
+
+
+# main()
