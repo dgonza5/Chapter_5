c78cba25f90007b3a9707e446ec73e55dfb2098c
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-05-03 15:55:36 -0700

    initilized chapter 5_3

---
+++ 5_3.py
@@ -0,0 +1,41 @@
+
+
+# translate = {
+#     "red": "rojo",
+#     "blue": "aloz",
+#     "green": "verdi",
+#     "white": "blanco",
+# }
+
+# alist = [
+#     "rojo",
+#     "aloz",
+#     "verdi",
+#     "blanco"
+# ]
+
+
+# print("alist", alist[1])
+# print("translate red", translate["white"])
+
+user = {
+    "first_name": "Robert",
+    "last_name": "Smith",
+    "age": 19
+    "school": {
+        "school_name": "Fresno State",
+        "grade": "Senior",
+        "gpa": 3.5,
+        "completed_courses": [
+            "Programming Fund",
+            "Project"
+        ]
+    }
+}
+
+
+dic1 = {
+    "a": "A",
+    "b": "B",
+    ""
+}
