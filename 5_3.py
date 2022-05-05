f12e5da461b47b41b672b17edd5dc5c3fe801d98
Deeno Gonzalez <deenogonzalez@DeeLuisaas-MBP.attlocal.net>
2022-05-05 13:59:50 -0700

    5_3 coding

--- 5_3.py
+++ 5_3.py
@@ -21,21 +21,82 @@
 user = {
     "first_name": "Robert",
     "last_name": "Smith",
-    "age": 19
+    "age": 19,
     "school": {
         "school_name": "Fresno State",
         "grade": "Senior",
         "gpa": 3.5,
         "completed_courses": [
             "Programming Fund",
-            "Project"
+            "Project Management",
+            "Psychology"
         ]
     }
 }
 
+# print(user["first_name"] + "'s age is: ", user["age"])
+# print("courses", user["school"]["completed_courses"])
+# num_courses = len(user["school"]["completed_courses"])
+# output = user["first_name"] + " goes to " + user["school"]["school_name"] + " and is taking " + str(num_courses) + " classes."
 
-dic1 = {
+# print(output)
+
+# if "completed_courses" in user:
+#     print("True")
+# else:
+#     print("False")
+
+# print("Original user: ", user)
+# user["school"] = None
+# print("Mutated user: ", user)
+
+# school = user.get("school", None)
+
+# print("School", school.get("completed_courses"))
+
+# print("keys: ", user.keys())
+# print("Values: ", user.values())
+
+# user_data = list(user)
+
+# print("User dictionary converted into a list: ", user_data)
+# print("User dictionary converted into a tuple of key value pairs: ", user.items())
+
+# print("User", user)
+# alist = [1, 2]
+
+# aset = set(alist)
+
+# del user["school"]
+
+# print("User with no school: ", user)
+
+# user.clear()
+
+# print("Cleared user object: ", user)
+original_dict = {}
+dict1 = {
     "a": "A",
     "b": "B",
-    ""
+    "c": "C"
 }
+
+dict2 = {
+    "c": "C",
+    "d": "D",
+    "e": "E",
+}
+
+# print("Merged Dictionary", original_dict.update(dict2)) # TODO: check python version
+
+def main():
+    print("Main running...")
+    d = {}
+    print(d)
+
+    d.update(dict2)
+    print("d", d)
+
+    print("Keys", d.keys())
+    print("Values", d.values())
+main()
